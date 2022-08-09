acc37afad680ade4d09c16c32bbcf31ea5254e88
Louis Hollingworth <louis@hollingworth.ch>
2022-08-09 13:19:10 +0100

    Almost ready for deployment. README needs correcting
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import configData from "../data/config.dev.json" assert { type: "json" };
-type Config = typeof configData;
-const config = configData as Config;
+import { readFileSync, writeFileSync } from "fs";
+import packagejson from "../package.json" assert { type: "json" };
+
+type Config = { subreddit_name: string; discord_webhook_url: string };
+
+const config = () => {
+	const path =
+		process.env.NODE_ENV === "development"
+			? "./data/config.dev.json"
+			: "./data/config.json";
+	const data: Config[] | Config = JSON.parse(readFileSync(path, "utf8"));
+
+	return process.env.NODE_ENV === "development"
+		? ([data] as Config[])
+		: (data as Config[]);
+};
 
 const fetchSubredditData = async (subreddit: string) => {
 	const response = await fetch(
@@ -25,7 +38,6 @@ const fetchSubredditData = async (subreddit: string) => {
 			}>;
 		};
 	} = await response.json();
-	console.log(json.data.children[0]);
 	return json.data.children[0].data;
 };
 
@@ -37,7 +49,7 @@ const sendToWebhook = async (subreddit: string) => {
 		title,
 		description: spoiler
 			? "This post is a spoiler"
-			: selftext.substring(0, 1000),
+			: selftext.substring(0, 50),
 		url: `https://reddit.com${permalink}`,
 		color: 0xf78316,
 		fields: [
@@ -56,7 +68,7 @@ const sendToWebhook = async (subreddit: string) => {
 		Object.assign(embed, { thumbnail: { url: thumbnail } });
 	}
 
-	await fetch(config.discord_webhook_url, {
+	await fetch(config()[0].discord_webhook_url, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -64,7 +76,56 @@ const sendToWebhook = async (subreddit: string) => {
 		body: JSON.stringify({
 			embeds: [embed],
 		}),
-	});
+	}).catch(console.error);
 };
 
-sendToWebhook(config.subreddit_name);
+const checkLastPost = async (subreddit: string, id: string) => {
+	try {
+		const data = await readFileSync("./data/subreddits.json", "utf8");
+
+		let parsed: Map<string, string> = JSON.parse(data);
+
+		if (parsed.get(subreddit) === id) return true;
+		else return false;
+	} catch (e: any) {
+		if (e.code === "ENOENT") {
+			fetchSubredditData(subreddit).then(async (data) => {
+				console.log(data);
+				sendToWebhook(subreddit);
+				writeFileSync(
+					"./data/subreddits.json",
+					`{"${subreddit}": "${data.id}"}`,
+					"utf8"
+				);
+			});
+		}
+	}
+};
+
+console.info(`Version: ${packagejson.version}`);
+
+fetchSubredditData(config()[0].subreddit_name).then(async (data) => {
+	if (await checkLastPost(config()[0].subreddit_name, data.id)) {
+		await sendToWebhook(config()[0].subreddit_name);
+		writeFileSync(
+			"./data/subreddits.json",
+			JSON.stringify({
+				[config()[0].subreddit_name]: data.id,
+			})
+		);
+	}
+});
+
+setInterval(() => {
+	fetchSubredditData(config()[0].subreddit_name).then(async (data) => {
+		if (await checkLastPost(config()[0].subreddit_name, data.id)) {
+			await sendToWebhook(config()[0].subreddit_name);
+			writeFileSync(
+				"./data/subreddits.json",
+				JSON.stringify({
+					[config()[0].subreddit_name]: data.id,
+				})
+			);
+		}
+	});
+}, 60000);
