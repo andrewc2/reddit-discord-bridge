88e37504803954943735204894ccd95eb84e9cb8
Louis Hollingworth <louis@hollingworth.ch>
2022-08-24 20:06:36 +0100

    Update config.js
    Add base url
    
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 export default {
 	title: "Reddit Discord Bridge",
 	description: "A project to post reddit posts to discord.",
+	base: "rdb.ludoviko.ch",
 	themeConfig: {
 		editLink: {
 			pattern: "https://github.com/Lucxjo/reddit-discord-bridge/edit/release/docs/:path",
